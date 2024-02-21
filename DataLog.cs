b3835531aa20fa6284be2683b53c7645b5566b26
chrsut01 <chrissu360@gmail.com>
2024-02-21 12:15:35 +0100

    init commit

---
+++ DataLog.cs
@@ -0,0 +1,11 @@
+﻿namespace DockerCalculator;
+
+public class DataLog
+{
+   
+    public double n1 { get; set; }
+    public double n2 { get; set; }
+    public string Operator { get; set; }
+    public double Result { get; set; }
+    
+}
