889e31c10252fc12cc475220967239b962941453
chrsut01 <chrissu360@gmail.com>
2024-02-23 10:09:18 +0100

    to fix version control connection

---
+++ Migrations/20240221124433_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DockerCalculator.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    n1 = table.Column<double>(type: "float", nullable: false),
+                    n2 = table.Column<double>(type: "float", nullable: false),
+                    Operator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Result = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataLogs");
+        }
+    }
+}
--- obj/Debug/net8.0/DockerCalculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/DockerCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca8b2f275f93e849625676b5e746268b48eb702")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
